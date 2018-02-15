e89e649c27745b47f4c6e70fafa415b283501a4d
pppereira3 <=>
2018-02-15 18:14:33 +0000

    Tarefa 1

--- src/lpoo/HelloWorld.java
+++ src/lpoo/HelloWorld.java
@@ -1,7 +1,38 @@
 package lpoo;
 
 public class HelloWorld {
+	
+	public static void printArray(char[] anArray) {
+		for (int i = 0; i < anArray.length; i++) {
+			if (i > 0) System.out.print(" ");
+	         System.out.print(anArray[i]);
+	      }
+	}
+	
+	public static void printMatrix(char[][] aMatrix) {
+		
+		for(int i = 0; i < aMatrix.length; i++) {
+			if(i > 0) System.out.print("\n");
+			printArray(aMatrix[i]);
+		}
+	}
+	
 	public static void main(String args[]) {
 		System.out.println("Hello World");
-	}
+		char tabuleiro[][] = {
+				{'X','X','X','X','X','X','X','X','X','X'},
+				{'X','H',' ',' ','I',' ','X',' ','G','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','X','X',' ','X','X','X','X',' ','X'},
+				{'X',' ','I',' ','I',' ','X','K',' ','X'},
+				{'X','X','X','X','X','X','X','X','X','X'}
+				};
+		
+			printMatrix(tabuleiro);
+		}
 }
+
