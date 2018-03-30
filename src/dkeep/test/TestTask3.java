ebd83e5cc93844c86963082f15f3f7bd7eeb12b7
teresaferreira <up201603811@fe.up.pt>
2018-03-30 17:02:23 +0100

    Iteration 3, task 3

---
+++ src/dkeep/test/TestTask3.java
@@ -0,0 +1,66 @@
+package dkeep.test;
+import static org.junit.Assert.*;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import dkeep.logic.Coords;
+import dkeep.logic.GameState;
+import dkeep.logic.Guard;
+import dkeep.logic.Ogre;
+
+public class TestTask3 {
+
+	char map[][] = {{ 'X', 'X', 'X', 'X', 'X'}, 
+	 		{ 'X', 'H', ' ', 'O', 'X'},
+	 		{ 'X', ' ', ' ', ' ', 'X'},
+	 		{ 'I', ' ', 'k', ' ', 'X'},
+	 		{ 'X', 'X', 'X', 'X', 'X'}};
+	
+	
+	@Test(timeout = 10000)
+	public void testOgreRandomBehaviour() {
+		boolean w = false, a = false, s = false, d = false, o = false;
+		LinkedList<Guard> gds = new LinkedList<Guard>();
+		LinkedList<Coords> ex = new LinkedList<Coords>();
+		LinkedList<Ogre> ogs = new LinkedList<Ogre>();
+		Coords co = new Coords(1, 3);
+		Ogre og = new Ogre('O', co, co, '*');
+		ogs.add(og);
+		GameState g = new GameState(map, ex, ogs, gds);
+		Coords cw, ca, cs, cd, coo;
+		while(!w || !a || !s || !d || !o) {
+			cw = new Coords(co.X() - 1, co.Y());
+			ca = new Coords(co.X(), co.Y() - 1);
+			cs = new Coords(co.X() + 1, co.Y());
+			cd = new Coords(co.X(), co.Y() + 1);
+			coo = new Coords(co.X(), co.Y());
+			g.getHero().moveHero(og.randommov(), g.getBoard());
+			g.moveOgres();
+			if(co.equals(cw)) {
+				w = true;
+			}else if(co.equals(ca)) {
+				a = true;
+			}else if(co.equals(cs)) {
+				s = true;
+			}else if(co.equals(cd)) {
+				d = true;
+			}else if(co.equal(coo)) {
+				o = true;
+			}
+			else {
+				fail("Ogre failed to move to the expected position");
+			}
+		}
+		System.out.println(w);
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(o);
+		assertTrue(a || s || d || w || o);
+	}
+	
+	
+	
+	
+}
